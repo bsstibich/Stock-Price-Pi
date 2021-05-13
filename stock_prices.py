c355311195445495148a8c73fc217e08fec41400
bsstibich <bsstibich@gmail.com>
2021-05-13 17:20:45 -0500

    functioning Object Oriented CLI version -Checks current and opening prices, compares and prints if up down or neutral -Next step is to change prints to lcd display and change comparison to led indicator

--- stock_prices.py
+++ stock_prices.py
@@ -1,23 +1,65 @@
 import yfinance as yf
-import os
+from os import system, name
 from time import sleep
+from ticker_class import stock
 
-def get_current_price(symbol):
-    ticker = yf.Ticker(symbol)
-    todays_data = ticker.history(period='1d')
-    return todays_data['Close'][0]
-def get_open_price(symbol):
-    ticker = yf.Ticker(symbol)
-    todays_data = ticker.history(period='1d')
-    return todays_data['Open'][0]
+def clear(): 						 
+    if name == 'nt': # for windows
+        _ = system('cls')  
+    else: # for mac and linux
+        _ = system('clear') 
 
+gmeT = 'GME'
+dogeT = 'DOGE-USD'
+btcT = 'BTC-USD'
+gme = stock(gmeT)
+
+btc = stock(btcT)
+
+doge = stock(dogeT)
 
 while True:
-    gme = get_current_price('GME')
-    doge = get_current_price('DOGE-USD')
-    btc = get_current_price('BTC-USD')
-    os.system('cls')
-    print("GME: " + str(gme))
-    print("DOGE: " + str(doge))
-    print("BTC: " + str(btc))
-    sleep(30)
+    gme.get_prices()
+    btc.get_prices()
+    doge.get_prices()
+
+    clear()
+
+    
+    
+    gme.output()
+    if(gme.current_price > gme.open_price):
+        print("up")
+    elif(gme.current_price < gme.open_price):
+        print("down")
+    else:
+        print("neutral")
+
+    btc.output()
+    if(btc.current_price > btc.open_price):
+        print("up")
+    elif(btc.current_price < btc.open_price):
+        print("down")
+    else:
+        print("neutral")
+
+    doge.output()
+    if(doge.current_price > doge.open_price):
+        print("up")
+    elif(doge.current_price < doge.open_price):
+        print("down")
+    else:
+        print("neutral")
+    
+    sleep(40)
+
+
+#while True:
+#    gme = get_current_price(gmeT)
+#    doge = get_current_price(dogeT)
+#   btc = get_current_price(btcT)
+#    os.system('cls')
+#    print("GME: " + str(gme))
+#    print("DOGE: " + str(doge))
+#    print("BTC: " + str(btc))
+#    sleep(30)
--- ticker_class.py
+++ ticker_class.py
@@ -3,20 +3,27 @@ from yfinance import ticker
 
 class stock():
     def __init__(self, ticker = ""):
+    
         self.ticker = ticker
-        self.open_price = 0
-        self.current_price = 0
+        self.open_price = self.get_open_price()
+        self.current_price = self.get_current_price()
 
     def get_current_price(self):
         ticker = yf.Ticker(self.ticker)
         todays_data = ticker.history(period='1d')
         self.current_price = todays_data['Close'][0]
+        return self.current_price
+        
     
     def get_open_price(self):
-        ticker = yf.Ticker(symbol)
+        ticker = yf.Ticker(self.ticker)
         todays_data = ticker.history(period='1d')
         self.open_price = todays_data['Open'][0]
+        return self.open_price
     
     def get_prices(self):
         self.get_open_price()
         self.get_current_price()
+    
+    def output(self):
+        print(self.ticker + ": " + str(self.current_price))
