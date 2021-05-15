3f9a19397174bd488ebcdbb57218e6511a601f1a
bsstibich <bsstibich@gmail.com>
2021-05-15 13:25:48 +0100

    Attempt to display actual stock prices on lcd -Stock.output() modified to display to lcd rather than just print -Stock.lcd_clear() added to manage display

--- ticker_class.py
+++ ticker_class.py
@@ -3,31 +3,34 @@ from yfinance import ticker
 import drivers
 
 class Stock():
-    def __init__(self, ticker = ""):
+	def __init__(self, ticker = ""):
     
-        self.ticker = ticker
-        self.open_price = self.get_open_price()
-        self.current_price = self.get_current_price()
-        self.display = drivers.Lcd()
+		self.ticker = ticker
+		self.open_price = self.get_open_price()
+		self.current_price = self.get_current_price()
+		self.display = drivers.Lcd()
 
-    def get_current_price(self):
-        ticker = yf.Ticker(self.ticker) #yfinance ticker definition
-        todays_data = ticker.history(period='1d') #get today's price history
-        self.current_price = todays_data['Close'][0] #get most recent closing price
-        return self.current_price
+	def get_current_price(self):
+		ticker = yf.Ticker(self.ticker) #yfinance ticker definition
+		todays_data = ticker.history(period='1d') #get today's price history
+		self.current_price = todays_data['Close'][0] #get most recent closing price
+		return self.current_price
         
     
-    def get_open_price(self):
-        ticker = yf.Ticker(self.ticker) #yfinance ticker definition
-        todays_data = ticker.history(period='1d') #get today's price history
-        self.open_price = todays_data['Open'][0] #get today's opening price
-        return self.open_price
+	def get_open_price(self):
+		ticker = yf.Ticker(self.ticker) #yfinance ticker definition
+		todays_data = ticker.history(period='1d') #get today's price history
+		self.open_price = todays_data['Open'][0] #get today's opening price
+		return self.open_price
+
+	def get_prices(self): #update both current and open prices
+		self.get_open_price()
+		self.get_current_price()
     
-    def get_prices(self): #update both current and open prices
-        self.get_open_price()
-        self.get_current_price()
-    
-    def output(self): #print current prices, will be adapted to LCD soon
-        print(self.ticker + ": " + str(self.current_price))
-        self.display.lcd_display_string("Hello World")
+	def output(self): #print current prices, will be adapted to LCD soon
+		print(self.ticker + ": " + str(self.current_price)) 
+		self.display.lcd_display_string(self.ticker + : , 1)
+		self.display.lcd_display_string(str(round(self.current_price,4)), 2)
+	def lcd_clear(self):
+		self.display.lcd_clear()
 
