62e20e6f05dd232f9b253f24d3ce621dd780bf50
bsstibich <bsstibich@gmail.com>
2021-05-17 13:59:23 -0500

    mend

--- stock_prices.py
+++ stock_prices.py
@@ -18,53 +18,53 @@ doge = Stock(dogeT)
 
 #running loop
 
-    while True:
-        try:
-            #get opening and current prices
-            gme.get_prices()
-            btc.get_prices()
-            doge.get_prices()
+while True:
+    try:
+        #get opening and current prices
+        gme.get_prices()
+        btc.get_prices()
+        doge.get_prices()
 
-            clear()
+        clear()
 
-            #print current price
-            gme.lcd_clear()
-            gme.output()
-            if(gme.current_price > gme.open_price):
-                print("up")
-            elif(gme.current_price < gme.open_price):
-                print("down")
-            else:
-                print("neutral")
-            sleep(5)
-            
+        #print current price
+        gme.lcd_clear()
+        gme.output()
+        if(gme.current_price > gme.open_price):
+            print("up")
+        elif(gme.current_price < gme.open_price):
+            print("down")
+        else:
+            print("neutral")
+        sleep(5)
+        
 
-            #print current price
-            btc.lcd_clear()
-            btc.output()
-            if(btc.current_price > btc.open_price):
-                print("up")
-            elif(btc.current_price < btc.open_price):
-                print("down")
-            else:
-                print("neutral")
-            sleep(5)
-            
-            
-            #print current price
-            doge.lcd_clear()
-            doge.output()
-            if(doge.current_price > doge.open_price):
-                print("up")
-            elif(doge.current_price < doge.open_price):
-                print("down")
-            else:
-                print("neutral")
-            sleep(5)
-        except:
-            btc.lcd_clear()
-            btc.display.lcd_display_string("Stock Prices Pi",1)
-            btc.display.lcd_display_string("Brandon Stibich",2)
-            
+        #print current price
+        btc.lcd_clear()
+        btc.output()
+        if(btc.current_price > btc.open_price):
+            print("up")
+        elif(btc.current_price < btc.open_price):
+            print("down")
+        else:
+            print("neutral")
+        sleep(5)
+        
+        
+        #print current price
+        doge.lcd_clear()
+        doge.output()
+        if(doge.current_price > doge.open_price):
+            print("up")
+        elif(doge.current_price < doge.open_price):
+            print("down")
+        else:
+            print("neutral")
+        sleep(5)
+    except:
+        btc.lcd_clear()
+        btc.display.lcd_display_string("Stock Prices Pi",1)
+        btc.display.lcd_display_string("Brandon Stibich",2)
+        
 
 
