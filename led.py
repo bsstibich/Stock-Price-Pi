ecfc1a107d7232395b0adfc660845cf0c4b2985c
bsstibich <bsstibich@gmail.com>
2021-05-17 16:08:18 -0500

    fix color swapping

--- led.py
+++ led.py
@@ -9,8 +9,8 @@ def setup():
     for i in pins:  # iterate on the RGB pins, initialize each and set to HIGH to turn it off (COMMON ANODE)
         GPIO.setup(i, GPIO.OUT, initial=GPIO.HIGH)
         #GPIO.setup(i, GPIO.HIGH)
-    pwmR = GPIO.PWM(pins[0], 2000)  # set each PWM pin to 2 KHz
-    pwmG = GPIO.PWM(pins[1], 2000)
+    pwmG = GPIO.PWM(pins[0], 2000)  # set each PWM pin to 2 KHz
+    pwmR = GPIO.PWM(pins[1], 2000)
     pwmB = GPIO.PWM(pins[2], 2000)
     pwmR.start(0)   # initially set to 0 duty cycle
     pwmG.start(0)
@@ -27,6 +27,8 @@ def red():
     setColor(100, 0, 0)
 def blue():
     setColor(0, 0, 100)
+def maroon():
+    setColor(50, 0, 0)
 def destroy():
     pwmR.stop()
     pwmG.stop()
