f44f71972fe2af12a26a04c0d32eb3ac0899a0c8
bsstibich <bsstibich@gmail.com>
2021-05-20 16:51:16 -0500

    display_stock() function added to stock_prices.py in order to reduce spaghetti

--- stock_prices.py
+++ stock_prices.py
@@ -8,6 +8,27 @@ def clear(): #CLI clear function
         _ = system('cls')  
     else: # for mac and linux
         _ = system('clear') 
+
+def display_stock(stock):
+        stock.lcd_clear()
+        stock.output()
+        try:
+            if(stock.current_price > stock.open_price):
+                print("up")
+                led.green()
+            elif(stock.current_price < stock.open_price):
+                print("down")
+                led.red()
+            else:
+                print("neutral")
+                led.blue()
+        except:
+            stock.current_price = "PRICE ERROR"
+            stock.open_price = "PRICE ERROR"
+            print("PRICE ERROR")
+            led.blue()
+        sleep(5)
+
 sleep(30)
 #init stocks
 gmeT = 'GME'
@@ -16,82 +37,31 @@ btcT = 'BTC-USD'
 gme = Stock(gmeT)
 btc = Stock(btcT)
 doge = Stock(dogeT)
+#init led
 led.setup()
 
 #running loop
-
 while True:
     try:
-        
         led.blue()
         gme.get_prices()
         btc.get_prices()
         doge.get_prices()
 
-
         clear()
-        gme.lcd_clear()
-        gme.output()
-        try:
-            if(gme.current_price > gme.open_price):
-                print("up")
-                led.green()
-            elif(gme.current_price < gme.open_price):
-                print("down")
-                led.red()
-            else:
-                print("neutral")
-                led.blue()
-        except:
-            gme.current_price = "PRICE ERROR"
-            gme.open_price = "PRICE ERROR"
-            led.blue()
-        sleep(5)
+        display_stock(gme)
+        display_stock(btc)
+        display_stock(doge)
 
-
-        btc.lcd_clear()
-        btc.output()
-        try:
-            if(btc.current_price > btc.open_price):
-                print("up")
-                led.green()
-            elif(btc.current_price < btc.open_price):
-                print("down")
-                led.red()
-            else:
-                print("neutral")
-                led.blue
-        except:
-            btc.current_price = "PRICE ERROR"
-            btc.open_price = "PRICE ERROR"
-            led.blue()
-        sleep(5)
-        
-        
-        doge.lcd_clear()
-        doge.output()'
-        try:
-            if(doge.current_price > doge.open_price):
-                print("up")
-                led.green()
-            elif(doge.current_price < doge.open_price):
-                print("down")
-                led.red()
-            else:
-                print("neutral")
-                led.blue()
-        except:
-            doge.current_price = "PRICE ERROR"
-            doge.open_price = "PRICE ERROR"
-            led.blue()
-        sleep(5)
     except KeyboardInterrupt:
+        print("Shutting Down stock_prices.py")
         break
     finally:
         btc.lcd_clear()
         btc.display.lcd_display_string("Stock Prices Pi",1)
         btc.display.lcd_display_string("Brandon Stibich",2)
         led.blue()
-led.destroy()    
+led.destroy()
+    
 
 
